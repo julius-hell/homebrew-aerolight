9971476b856510d93ab1810ad7aaab2a1ce33597
Julius Hell <mail@jhell.dev>
2025-10-26 15:39:54 +0100

    updated version

--- Casks/aerolight.rb
+++ Casks/aerolight.rb
@@ -1,5 +1,5 @@
 cask "aerolight" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "385b40c77209dfd08254f33a2639b2af935d82df5e8bb688520996639360a13c" # SHA256 deines .app.zip
 
   url "https://github.com/julius-hell/aerolight/releases/download/v#{version}/Aerolight.app.zip"
