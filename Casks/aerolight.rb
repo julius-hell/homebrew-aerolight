b58154024ecdb1669e324bc3dfc9dc8704a3343e
Julius Hell <mail@jhell.dev>
2025-10-27 17:57:13 +0100

    switched to swift app

--- Casks/aerolight.rb
+++ Casks/aerolight.rb
@@ -1,17 +1,23 @@
 cask "aerolight" do
-  version "1.0.5"
-  sha256 "e7a455c561cf493d58fed6f0e74ceccfa31c668b9db422b75cb34044c02c711c"
+  version "1.0.0"
+  sha256 :no_check
 
-  url "https://github.com/julius-hell/aerolight/releases/download/v#{version}/Aerolight.app.zip"
+  url "https://github.com/julius-hell/aerolight-app/releases/download/v#{version}/aerolight-app-#{version}.zip"
   name "Aerolight"
-  desc "Window switcher for macOS"
-  homepage "https://github.com/julius-hell/aerolight"
+  desc "Window switcher for AeroSpace window manager on macOS"
+  homepage "https://github.com/julius-hell/aerolight-app"
 
-  app "Aerolight.app"
-  binary "#{appdir}/Aerolight.app/Contents/MacOS/Aerolight", target: "aerolight"
+  app "aerolight-app.app"
+
+  binary "#{appdir}/aerolight-app.app/Contents/MacOS/aerolight-app", target: "aerolight"
 
   postflight do
     system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/Aerolight.app"]
+                   args: ["-cr", "#{appdir}/aerolight-app.app"]
   end
+
+  zap trash: [
+    "~/Library/Preferences/com.aerolight-app.plist",
+    "~/Library/Saved Application State/com.aerolight-app.savedState",
+  ]
 end
